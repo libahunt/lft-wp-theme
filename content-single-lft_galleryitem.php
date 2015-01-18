a0cc1c94a5c6c7d3862afa5e04f07d93576ded97
Anna Jõgi <annaj6gi@gmail.com>
2015-01-18 23:54:28 +0200

    important fixes and GA

--- content-single-lft_galleryitem.php
+++ content-single-lft_galleryitem.php
@@ -7,12 +7,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php echo the_post_thumbnail(); ?>
 
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		<?php the_title( '<h1 class="entry-title lft-galleryitem-title">', '</h1>' ); ?>
 		<?php the_content(); ?>
 		<?php
 			/*wp_link_pages( array(
